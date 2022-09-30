a95dece8ed09fb470c659cdc5f15110d6abe432c
AntonC9018 <anton.curm@gmail.com>
2022-09-30 23:55:45 +0300

    display works

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -15,40 +15,66 @@ class Program
     static void Main()
     {
         int[] array;
-        switch (ReadInitializationKind())
+        switch (InputHelper.ReadInitializationKind())
         {
             case null:
                 return;
             case ArrayInitializationKind.Random:
             {
-                int? size = GetPositiveNumber("What should be the size of the array? ");
+                int? size = InputHelper.GetPositiveNumber("What should be the size of the array? ");
                 if (!size.HasValue)
                     return;
                 
-                int? min = GetPositiveNumber("What should be the minimum value? ");
-                if (!min.HasValue)
-                    return;
+                #if false
+                {
+                    int? min = InputHelper.GetPositiveNumber("What should be the minimum value? ");
+                    if (!min.HasValue)
+                        return;
 
-                int? max = GetPositiveNumber("What should be the maximum value? ");
-                if (!max.HasValue)
-                    return;
+                    // We need the values to be unique.
+                    int? max;
+                    {
+                        int lowerBound = min.Value + size.Value;
+                        var lowerBoundConstraint = new InputHelper.LowerBoundConstraint(lowerBound);
+                        var constraints = new InputHelper.IInputConstraint<int>[] { lowerBoundConstraint };
+                        max = InputHelper.GetNumberWithConstraints($"What should be the maximum value? (> {lowerBound}) ", constraints);
+                        if (!max.HasValue)
+                            return;
+                    }
 
+                    var rand = new Random(808080);
+                    var valuesMet = new HashSet<int>();
+                    array = new int[size.Value];
+                    foreach (ref int v in array.AsSpan())
+                    {
+                        int t;
+                        // Keep generating random values until we get one that has not been encountered before.
+                        // We are guaranteed to be able to fill up the array randomly, because 
+                        while (!valuesMet.Add(
+                            t = rand.Next(min.Value, max.Value)))
+                        {
+                            v = t;
+                        }
+                    }
+                }
+                #endif
+
+                array = Enumerable.Range(0, size.Value).ToArray();
                 var rand = new Random(808080);
-                array = new int[size.Value];
-                foreach (ref int v in array.AsSpan())
-                    v = rand.Next(min.Value, max.Value);
+                rand.Shuffle(array.AsSpan());
+                
                 break;
             }
             case ArrayInitializationKind.Console:
             {
-                int? size = GetPositiveNumber("What should be the size of the array? ");
+                int? size = InputHelper.GetPositiveNumber("What should be the size of the array? ");
                 if (!size.HasValue)
                     return;
 
                 array = new int[size.Value];
                 for (int i = 0; i < size.Value; i++)
                 {
-                    int? input = GetPositiveNumber($"array[{i}] = ");
+                    int? input = InputHelper.GetPositiveNumber($"array[{i}] = ");
                     if (!input.HasValue)
                         return;
                     
@@ -65,36 +91,58 @@ class Program
 
         {
             var comparer = Comparer<int>.Default;
-            var display = new Display();
+            var display = new ThreadSleep_ConsoleCursor_Display();
             while (true)
             {
-                Console.WriteLine("The array: " + string.Join(',', array.Select(i => i.ToString())));
+                void WriteArray(int[] arr) => Console.WriteLine("The array: " + string.Join(',', arr.Select(i => i.ToString())));
+                
+                WriteArray(array);
                 Console.Write("Enter the sort kind to perform (quick).");
                 string? input = Console.ReadLine();
+                display.SetDrawToCurrentPosition();
                 switch (input)
                 {
                     case "quick":
                     {
                         var arrCopy = array.ToArray();
-                        Sorting.QuckSort(arrCopy, comparer, display);
-                        Console.WriteLine("The array: " + string.Join(',', arrCopy.Select(i => i.ToString())));
-                        continue;
+                        display.ArrayRef = arrCopy;
+                        Sorting.QuickSort(arrCopy, comparer, display);
+                        display.CompleteDrawing();
+                        WriteArray(arrCopy);
+                        break;
                     }
+
                     case null:
                         return;
                     
                     default:
                     {
                         Console.WriteLine(input + " is not a valid option.");
-                        continue;
+                        break;
                     }
                 }
+
             }
         }
 
     }
+}
 
-    static ArrayInitializationKind? ReadInitializationKind()
+public static class RandomHelper
+{
+    public static void Shuffle<T>(this Random rand, Span<T> array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            int j = rand.Next(0, array.Length);
+            Helper.Swap(ref array[i], ref array[j]);
+        }
+    }
+}
+
+public static class InputHelper
+{
+    public static ArrayInitializationKind? ReadInitializationKind()
     {
         while (true)
         {
@@ -115,7 +163,48 @@ class Program
         }
     }
 
-    static int? GetPositiveNumber(string message)
+    public class PositiveNumberConstraint : IInputConstraint<int>
+    {
+        public static readonly PositiveNumberConstraint Instance = new();
+        private PositiveNumberConstraint(){}
+        public bool Check(int parsedValue)
+        {
+            return parsedValue >= 0;
+        }
+
+        public string FormatError(string rawValue, int parsedValue)
+        {
+            return rawValue + " should be positive.";
+        }
+    }
+
+    public class LowerBoundConstraint : IInputConstraint<int>
+    {
+        public int LowerBound { get; set; }
+
+        public LowerBoundConstraint(int lowerBound)
+        {
+            LowerBound = lowerBound;
+        }
+
+        public bool Check(int parsedValue)
+        {
+            return parsedValue >= LowerBound;
+        }
+
+        public string FormatError(string rawValue, int parsedValue)
+        {
+            return rawValue + " should be larger than " + LowerBound;
+        }
+    }
+
+    private static readonly IInputConstraint<int>[] PositiveNumberConstraints = { PositiveNumberConstraint.Instance };
+    public static int? GetPositiveNumber(string message)
+    {
+        return GetNumberWithConstraints(message, PositiveNumberConstraints);
+    }
+
+    public static int? GetNumberWithConstraints(string message, ReadOnlySpan<IInputConstraint<int>> constraints)
     {
         while (true)
         {
@@ -129,11 +218,18 @@ class Program
                 {
                     if (int.TryParse(input, out int value))
                     {
-                        if (value < 0)
+                        bool bad = false;
+                        foreach (var constraint in constraints)
                         {
-                            Console.WriteLine("The values should be positive");
-                            continue;
+                            if (!constraint.Check(value))
+                            {
+                                bad = true;
+                                var error = constraint.FormatError(input, value);
+                                Console.WriteLine(error);
+                            }
                         }
+                        if (bad)
+                            continue;
                         return value;
                     }
 
@@ -143,6 +239,31 @@ class Program
             }
         }
     }
+
+    public interface IInputConstraint<in T>
+    {
+        string FormatError(string rawValue, T? parsedValue);
+        bool Check(T parsedValue);
+    }
+}
+
+public static class Helper
+{
+    public static void Swap<T>(ref T a, ref T b)
+    {
+        T t = a;
+        a = b;
+        b = t;
+    }
+
+    public static int GetNumDecimalDigits(int value)
+    {
+        int numDigits = 1;
+        int current = value;
+        while ((current /= 10) != 0)
+            numDigits++;
+        return numDigits;
+    }
 }
 
 
@@ -150,96 +271,131 @@ public static class Sorting
 {
     internal ref struct SortContext<T>
     {
-        public Span<T> Span;
+        public Span<T> WholeSpan;
+        public int StartIndex;
+        public int SpanLength;
+        public readonly Span<T> Span => WholeSpan.Slice(StartIndex, SpanLength);
+
         public IComparer<T> Comparer;
         public IDisplay<T> Display;
 
-        public readonly void Swap(int a, int b)
+        public readonly void SwapLocal(int localIndex0, int localIndex1)
         {
-            Display.RecordSwap(Span, a, b);
-            Sorting.Swap(ref Span[a], ref Span[b]);
+            Swap(localIndex0 + StartIndex, localIndex1 + StartIndex);
+        }
+
+        public readonly int CompareLocal(int localIndex0, int localIndex1)
+        {
+            return Compare(localIndex0 + StartIndex, localIndex1 + StartIndex);
+        }
+
+        public readonly void Swap(int index0, int index1)
+        {
+            Display.BeginSwap(index0, index1);
+            Helper.Swap(ref WholeSpan[index0], ref WholeSpan[index1]);
+            Display.EndSwap(index0, index1);
+        }
+
+        public readonly int Compare(int index0, int index1)
+        {
+            int comparisonResult = Comparer.Compare(WholeSpan[index0], WholeSpan[index1]);
+            Display.RecordComparison(index0, index1, comparisonResult);
+            return comparisonResult;
         }
     } 
 
-    internal static void Swap<T>(ref T a, ref T b)
+    internal static int quick_partition<T>(in SortContext<T> context)
     {
-        T t = a;
-        a = b;
-        b = t;
-    }
-
-    internal static int quick_partition<T>(SortContext<T> context)
-    {
-        var pivotValue = context.Span[^1];
+        var span = context.Span;
+        var lastIndex = span.Length - 1;
         var pivotIndex = 0;
-        int begin = 0;
 
-        while (context.Comparer.Compare(context.Span[pivotIndex], pivotValue) != 0)
+        for (int leftIndex = 0; leftIndex < lastIndex; leftIndex++)
         {
-            int comparisonResult = context.Comparer.Compare(context.Span[pivotIndex], pivotValue);
-            context.Display.RecordComparisonValueIndex(context.Span, pivotValue, pivotIndex, comparisonResult);
-
-            if (comparisonResult < 0)
+            if (context.CompareLocal(leftIndex, lastIndex) < 0)
             {   
-                context.Swap(pivotIndex, begin);
+                context.SwapLocal(pivotIndex, leftIndex);
                 pivotIndex++;
             }
-            begin++;
         }
 
-        context.Swap(context.Span.Length - 1, pivotIndex);
+        context.SwapLocal(span.Length - 1, pivotIndex);
 
         return pivotIndex;
     }
 
-    internal static void quick_sort<T>(SortContext<T> context)
+    internal static void quick_sort<T>(ref SortContext<T> context)
     {
-        if (context.Span.Length == 0)
+        if (context.SpanLength <= 1)
             return;
 
-        context.Display.RecordIteration(context.Span);
+        context.Display.RecordIteration();
 
         int partitionIndex = quick_partition(context);
 
-        var contextTemp = context;
+        int ownStart = context.StartIndex;
+        int ownLength = context.SpanLength;
         {
-            contextTemp.Span = context.Span[.. partitionIndex];
-            quick_sort(contextTemp);
+            context.StartIndex = ownStart;
+            context.SpanLength = partitionIndex;
+            quick_sort(ref context);
         }
         {
-            contextTemp.Span = context.Span[(partitionIndex + 1) ..];
-            quick_sort(contextTemp);
+            context.StartIndex = ownStart + partitionIndex + 1;
+            context.SpanLength = ownLength - (partitionIndex + 1);
+            quick_sort(ref context);
         }
     }
 
-    public static void QuckSort<T>(
+    public static void QuickSort<T>(
         Span<T> span,
         IComparer<T> comparer,
         IDisplay<T> display)
     {
-        quick_sort<T>(new()
+        var context = new SortContext<T>
         {
-            Span = span,
             Comparer = comparer,
-            Display = display,
-        });
+            Display = display, 
+            WholeSpan = span,
+            SpanLength = span.Length,
+            StartIndex = 0,
+        };
+        quick_sort<T>(ref context);
     }
 }
 
 public interface IDisplay<T>
 {
-    void RecordIteration(ReadOnlySpan<T> state);
-    void RecordComparisonIndexIndex(ReadOnlySpan<T> state, int index0, int index1, int comparisonResult);
-    void RecordComparisonValueIndex(ReadOnlySpan<T> state, T value, int index, int comparisonResult);
-    void RecordComparisonValueValue(ReadOnlySpan<T> state, T value0, T value1, int comparisonResult);
-    void RecordSwap(ReadOnlySpan<T> state, int index0, int index1);
+    void RecordIteration();
+    void RecordComparison(int index0, int index1, int comparisonResult);
+    void BeginSwap(int index0, int index1);
+    void EndSwap(int index0, int index1);
 }
 
-public class Display : IDisplay<int>
+public static class DisplayHelper
 {
-    private int _iterationCount;
-    private int _comparisonCount;
-    private int _swapCount;
+    public struct Counters
+    {
+        public int Iteration;
+        public int Comparison;
+        public int Swap;
+    }
+}
+
+public sealed class PrintAllOnNewLinesDisplay : IDisplay<int>
+{
+    private DisplayHelper.Counters _counters;
+    public int[] ArrayRef { get; set; }
+
+    public PrintAllOnNewLinesDisplay(int[] arrayRef)
+    {
+        ArrayRef = arrayRef;
+    }
+
+    public void ResetCounters()
+    {
+        _counters = default;
+    }
 
     private void WriteState(ReadOnlySpan<int> state)
     {
@@ -251,66 +407,131 @@ public class Display : IDisplay<int>
         }
     }
 
-    private void WriteStateSwapped(ReadOnlySpan<int> state, int a, int b)
+    public void RecordIteration()
     {
-        for (int i = 0; i < state.Length; i++)
+        Console.Write($"Iteration {_counters.Iteration++}.");
+        WriteState(ArrayRef);
+        Console.WriteLine();
+    }
+
+    public void RecordComparison(int index0, int index1, int comparisonResult)
+    {
+        Console.WriteLine($"Comparison {_counters.Comparison++}: {ArrayRef[index0]} > {ArrayRef[index1]} = {comparisonResult}.");
+        WriteState(ArrayRef);
+        Console.WriteLine();
+    }
+
+    public void BeginSwap(int index0, int index1)
+    {
+        Console.WriteLine($"Swap {_counters.Swap++}. [{index0}] <-> [{index1}].");
+        WriteState(ArrayRef);
+        Console.WriteLine();
+    }
+
+    public void EndSwap(int index0, int index1)
+    {
+        WriteState(ArrayRef);
+        Console.WriteLine();
+    }
+}
+
+public sealed class ThreadSleep_ConsoleCursor_Display : IDisplay<int>
+{
+    private int[]? _arrayRef;
+    private int _cachedElementWidth;
+
+    public int[]? ArrayRef
+    {
+        get
         {
-            if (i != 0)
-                Console.Write(", ");
-            if (i == a)
-                Console.Write(state[b]);
-            else if (i == b)
-                Console.Write(state[a]);
-            else 
-                Console.Write(state[i]);
+            return _arrayRef;
+        }
+        set
+        {
+            if (value is not null)
+                _cachedElementWidth = Helper.GetNumDecimalDigits(value.Max());
+            _arrayRef = value;
         }
     }
 
-    public void RecordIteration(ReadOnlySpan<int> state)
+    public struct DrawPosition
     {
-        Console.Write($"Iteration {_iterationCount++}. State: ");
-        WriteState(state);
-        Console.WriteLine();
+        public int Top;
+        public int Left;
+    }
+    public DrawPosition? Position { get; set; }
+
+    public void SetDrawToCurrentPosition()
+    {
+        Position = new()
+        {
+            Top = Console.CursorTop,
+            Left = Console.CursorLeft,
+        };
     }
 
-    public void RecordSwap(ReadOnlySpan<int> state, int index0, int index1)
+    public void CompleteDrawing()
     {
-        Console.WriteLine($"Swap {_swapCount++}. [{index0}] <-> [{index1}].");
-        
-        Console.Write("State before: ");
-        WriteState(state);
-        Console.WriteLine();
-
-        Console.Write("State after: ");
-        WriteStateSwapped(state, index0, index1);
-        Console.WriteLine();
+        Debug.Assert(Position.HasValue);
+        var pos = Position.Value;
+        Console.CursorTop = pos.Top + 1;
+        Console.CursorLeft = 0;
+        Position = null;
     }
 
-    public void RecordComparisonIndexIndex(ReadOnlySpan<int> state, int index0, int index1, int comparisonResult)
+    private static void DrawState(DrawPosition position, int elementWidth, ReadOnlySpan<int> array)
     {
-        RecordComparisonValueValue(state, state[index0], state[index1], comparisonResult);
+        Console.CursorTop = position.Top;
+        Console.CursorLeft = position.Left;
+        Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i != 0)
+                Console.Write(" ");
+            int value = array[i];
+            string strValue = value.ToString().PadLeft(elementWidth);
+            Console.Write(strValue);
+        }
+        Console.Write("]");
     }
 
-    public void RecordComparisonValueIndex(ReadOnlySpan<int> state, int value, int index, int comparisonResult)
+    private void AssertInitialized()
     {
-        RecordComparisonValueValue(state, value, state[index], comparisonResult);
+        Debug.Assert(_arrayRef is not null);
+        Debug.Assert(Position.HasValue);
     }
 
-    public void RecordComparisonValueValue(ReadOnlySpan<int> state, int value0, int value1, int comparisonResult)
+    private void DrawAndSleep(int sleepDuration)
     {
-        Console.WriteLine($"Comparison {_iterationCount++}: {value0} > {value1} = {comparisonResult}.");
+        AssertInitialized();
+        DrawState(Position.Value, _cachedElementWidth, _arrayRef);
+        Thread.Sleep(sleepDuration);
+    }
+
+    public void BeginSwap(int index0, int index1)
+    {
+        // DrawAndSleep(1000);
+    }
+
+    public void EndSwap(int index0, int index1)
+    {
+        if (index0 != index1)
+            DrawAndSleep(1000);
+    }
+
+    public void RecordComparison(int index0, int index1, int comparisonResult)
+    {
+        // DrawAndSleep(1000);
+    }
+
+    public void RecordIteration()
+    {
+        // DrawAndSleep(1000);
     }
 }
 
-public class IntComparer : IComparer<int>
-{
-    public int Compare(int x, int y)
-    {
-        return x.CompareTo(y);
-    }
-}
 
-enum ArrayInitializationKind
+public enum ArrayInitializationKind
 {
     Random,
     Console,
