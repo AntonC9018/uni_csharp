1899bca47ccd98dd7c28a29d498bba50dc5fcec8
AntonC9018 <anton.curm@gmail.com>
2023-03-14 23:45:23 +0200

    lab3

---
+++ sem2_lab3/MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace sem2_lab3;
+
+public partial class StudentModel : ObservableObject
+{
+    [ObservableProperty] private string? _firstName;
+    [ObservableProperty] private string? _lastName;
+    [ObservableProperty] private float _averageGrade;
+    [ObservableProperty] private string? _strongestSubject;
+}
+
+public class ClassroomDataModel
+{
+    public ObservableCollection<StudentModel> Students { get; } = new();
+    public event Action? StudentsChangedInAnyWay;
+
+    public ClassroomDataModel()
+    {
+        Students.CollectionChanged += (_, args) =>
+        {
+            if (args.NewItems is not null)
+            {
+                foreach (var item in args.NewItems)
+                    ((StudentModel) item).PropertyChanged += (_, _) => StudentsChangedInAnyWay?.Invoke();
+            }
+            StudentsChangedInAnyWay?.Invoke();
+        };
+    }
+}
+
+public partial class MainWindowViewModel : ObservableObject, IDisposable
+{
+    public ClassroomDataModel Model { get; }
+    private Random _random = new();
+    
+    [NotifyCanExecuteChangedFor(nameof(RerunCurrentQueryCommand))]
+    [ObservableProperty]
+    private int? _selectedQueryIndex;
+
+    public record struct QueryResultRecord(QueryResultKind Kind, object Value);
+    [ObservableProperty] private QueryResultRecord _queryResult;
+    [ObservableProperty] private bool _autoRerun = true;
+
+    public MainWindowViewModel(ClassroomDataModel model)
+    {
+        Model = model;
+        model.StudentsChangedInAnyWay += RerunCurrentQuery;
+    }
+
+    public void Dispose()
+    {
+        Model.StudentsChangedInAnyWay -= RerunCurrentQuery;
+    }
+
+    partial void OnSelectedQueryIndexChanged(int? value)
+    {
+        RerunCurrentQuery();
+    }
+    
+    // Might need to wrap this in a view model for rows
+    public ObservableCollection<StudentModel> StudentList => Model.Students;
+    
+    private static readonly string[] _Names = {"John", "Jane", "Bob", "Alice", "Joe", "Mary", "Jack", "Jill"};
+    private static readonly string[] _LastNames = 
+        { "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore" };
+    private static readonly string[] _Subjects = {"Math", "English", "Science", "History", "Art", "Music", "PE"};
+    
+    [RelayCommand]
+    private void SpawnRandomStudent()
+    {
+        var student = new StudentModel
+        {
+            FirstName = _Names[_random.Next(_Names.Length)],
+            LastName = _LastNames[_random.Next(_LastNames.Length)],
+            AverageGrade = _random.Next(3, 10),
+            StrongestSubject = _Subjects[_random.Next(_Subjects.Length)]
+        };
+        Model.Students.Add(student);
+    }
+    
+    public IEnumerable<string> QueryNames => Queries.QueryInfos.Select(q => q.Name);
+
+    [RelayCommand(CanExecute = nameof(CanRerunCurrentQuery))]
+    private void RerunCurrentQuery()
+    {
+        if (SelectedQueryIndex is null)
+            return;
+        if (!AutoRerun)
+            return;
+        var qinfo = Queries.QueryInfos[SelectedQueryIndex.Value];
+        var query = qinfo.Method;
+        QueryResult = new(qinfo.Kind, query(Model.Students));
+    }
+    
+    public bool CanRerunCurrentQuery => SelectedQueryIndex is not null;
+}
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, IDisposable
+{
+    public MainWindowViewModel ViewModel => (MainWindowViewModel) DataContext;
+    
+    public MainWindow()
+    {
+        var model = new ClassroomDataModel();
+        model.Students.Add(new StudentModel
+        {
+            FirstName = "John",
+            LastName = "Smith",
+            AverageGrade = 5.5f,
+            StrongestSubject = "Math"
+        });
+        DataContext = new MainWindowViewModel(model);
+        InitializeComponent();
+        
+        ViewModel.PropertyChanged += (object? sender, PropertyChangedEventArgs args) =>
+        {
+            switch (args.PropertyName)
+            {
+                case nameof(ViewModel.QueryResult):
+                {
+                    RefreshQueryResultView();
+                    break;
+                }
+            }
+        };
+    }
+
+    public void Dispose() => ViewModel.Dispose();
+
+    private void RefreshQueryResultView()
+    {
+        var r = ViewModel.QueryResult;
+        
+        void Refresh<T>(Func<T> creator, Action<T> modify) where T : UIElement
+        {
+            if (QueryResultView.Children is [T widget])
+            {
+                modify(widget);
+                return;
+            }
+            QueryResultView.Children.Clear();
+            widget = creator();
+            modify(widget);
+            QueryResultView.Children.Add(widget);
+        }
+
+        void RefreshText(string? text)
+        {
+            Refresh(() => new TextBlock
+                {
+                    FontSize = 24,
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Center
+                },
+                tb => tb.Text = text ?? "No results.");
+        }
+
+        bool MaybeNoResults(IEnumerable enumerable)
+        {
+            var e = enumerable.GetEnumerator();
+            using var _ = e as IDisposable;
+            if (!e.MoveNext())
+            {
+                RefreshText(null);
+                return true;
+            }
+            return false;
+        }
+        
+        switch (r.Kind)
+        {
+            case QueryResultKind.SingleValue:
+            {
+                RefreshText(r.Value!.ToString());
+                break;
+            }
+            case QueryResultKind.ComplexValue:
+            {
+                throw new NotImplementedException();
+            }
+            case QueryResultKind.MultipleSimpleValues:
+            {
+                if (MaybeNoResults((IEnumerable) r.Value!))
+                    break;
+                Refresh(() => new ListBox
+                    {
+                        HorizontalAlignment = HorizontalAlignment.Stretch,
+                        VerticalAlignment = VerticalAlignment.Stretch
+                    },
+                    lb => lb.ItemsSource = (IEnumerable) r.Value!);
+                break;
+            }
+            case QueryResultKind.MultipleComplexValues:
+            {
+                if (MaybeNoResults((IEnumerable) r.Value!))
+                    break;
+                Refresh(() => new DataGrid
+                    {
+                        IsReadOnly = true,
+                        AutoGenerateColumns = true,
+                        HorizontalAlignment = HorizontalAlignment.Stretch,
+                        VerticalAlignment = VerticalAlignment.Stretch
+                    },
+                    lb => lb.ItemsSource = (IEnumerable) r.Value!);
+                break;
+            }
+        }
+    }
+}
---
+++ sem2_lab3/Queries.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace sem2_lab3;
+
+public enum QueryResultKind
+{
+    SingleValue,
+    MultipleSimpleValues,
+    ComplexValue,
+    MultipleComplexValues,
+}
+
+public static class Queries
+{
+    public record struct QueryInfo(string Name, QueryResultKind Kind, Func<IEnumerable<StudentModel>, object> Method);
+    public static readonly QueryInfo[] QueryInfos = typeof(Queries)
+        .GetMethods(BindingFlags.Static | BindingFlags.Public)
+        .Select(m =>
+        {
+            QueryResultKind GetKind()
+            {
+                var t = m.ReturnType;
+                
+                if (t == typeof(string))
+                    return QueryResultKind.SingleValue;
+                
+                if (!t.IsAssignableTo(typeof(IEnumerable)))
+                    return t.IsClass ? QueryResultKind.ComplexValue : QueryResultKind.SingleValue;
+
+                var maybeSelfAsInterface = new List<Type>();
+                if (t.IsInterface)
+                    maybeSelfAsInterface.Add(t);
+                var enumerableType = maybeSelfAsInterface.Concat(t.GetInterfaces())
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+                    .Select(i => i.GetGenericArguments()[0])
+                    .FirstOrDefault() ?? typeof(object);
+
+                if (enumerableType.IsClass && enumerableType != typeof(string))
+                    return QueryResultKind.MultipleComplexValues;
+                
+                return QueryResultKind.MultipleSimpleValues;
+            }
+
+            var kind = GetKind();
+            
+            return new QueryInfo(m.Name, kind, e =>
+            {
+                object result = m.Invoke(null, new object[] { e })!;
+                switch (kind)
+                {
+                    case QueryResultKind.MultipleComplexValues:
+                    case QueryResultKind.MultipleSimpleValues:
+                        return ((IEnumerable) result).Cast<object>().ToArray();
+                    
+                    default:
+                        return result;
+                }
+            });
+        })
+        .ToArray();
+    
+    private static string GetFullName(StudentModel s) => s.FirstName + " " + s.LastName;
+    
+    public static IEnumerable<StudentModel> GetStudentsWithHighGrade(IEnumerable<StudentModel> students)
+    {
+        return from student in students
+            where student.AverageGrade >= 8
+            select student;
+    }
+    
+    public static IEnumerable<StudentModel> GetStudentsWithLowGrade(IEnumerable<StudentModel> students)
+    {
+        return from student in students
+            where student.AverageGrade < 8
+            select student;
+    }
+    
+    public static IEnumerable<string> GetStudentNames(IEnumerable<StudentModel> students)
+    {
+        return from student in students
+            select GetFullName(student);
+    }
+
+    public static string GetMostCommonStrongSubject(IEnumerable<StudentModel> students)
+    {
+        return (from student in students
+            group student by student.StrongestSubject into g
+            orderby g.Count() descending
+            select g.Key).First();
+    }
+    
+    public record StudentWithStrongestSubject(string Names, string Subject);
+    public static IEnumerable<StudentWithStrongestSubject> GetStudentNamesWithStrongestSubject(IEnumerable<StudentModel> students)
+    {
+        return from student in students
+            let fullName = GetFullName(student)
+            group fullName by student.StrongestSubject into g
+            orderby g.Key
+            select new StudentWithStrongestSubject(string.Join(", ", g), g.Key);
+        
+        // return students.Select(student => new { student, fullName = GetFullName(student) })
+        //     .GroupBy(@t => @t.student.StrongestSubject, @t => @t.fullName)
+        //     .OrderBy(g => g.Key)
+        //     .Select(g => new StudentWithStrongestSubject(string.Join(", ", g), g.Key));
+    }
+    
+    // Query where students are joined with themselves
+    public record StudentPair(string Student1Name, string Student2Name);
+    public static IEnumerable<StudentPair> GetGoodStudentPairs(IEnumerable<StudentModel> students)
+    {
+        return from student in students
+            join student2 in students
+                on student.StrongestSubject equals student2.StrongestSubject
+            where student != student2
+            select new StudentPair(GetFullName(student), GetFullName(student2));
+    }
+    
+    // First name must start with J
+    public static IEnumerable<string> GetStudentNamesWhereFirstNameHasJ(IEnumerable<StudentModel> students)
+    {
+        return from student in students
+            where student.FirstName.StartsWith("J", StringComparison.CurrentCultureIgnoreCase)
+            select GetFullName(student);
+    }
+    
+    // Last name must contain "OO"
+    public static IEnumerable<string> GetStudentNamesWhereLastNameHasOO(IEnumerable<StudentModel> students)
+    {
+        return from student in students
+            where student.LastName.Contains("OO", StringComparison.CurrentCultureIgnoreCase)
+            select GetFullName(student);
+    }
+    
+    // Average grade must be between 5 and 7
+    public static IEnumerable<string> GetStudentNamesWithAverageGradeBetween5And7(IEnumerable<StudentModel> students)
+    {
+        return from student in students
+            where student.AverageGrade is >= 5 and <= 7
+            select GetFullName(student);
+    }
+    
+    // Students count whose names are the same
+    public static int GetStudentsCountWithSameFirstName(IEnumerable<StudentModel> students)
+    {
+        return (from student in students
+            group student by student.FirstName into g
+            where g.Count() > 1
+            select g).Count();
+    }
+    
+    public record SubjectAndAverageGrade(string Subject, float AverageGrade);
+    
+    public static IEnumerable<SubjectAndAverageGrade> GetWeakestSubjectByGrade(IEnumerable<StudentModel> students)
+    {
+        return from student in students
+            group student by student.StrongestSubject into g
+            let gradeAverage = g.Average(s => s.AverageGrade)
+            orderby gradeAverage descending
+            select new SubjectAndAverageGrade(g.Key, gradeAverage);
+    }
+}
