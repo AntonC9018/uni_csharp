3ae84bed01abdf9c17b92f90a4461346a7a7fc1c
AntonC9018 <anton.curm@gmail.com>
2023-04-02 13:11:16 +0300

    referat test

---
+++ sem2_referat/code/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+bool EqualsCheck<T>(T a, T b)
+{
+    var comparer = EqualityComparer<T>.Default;
+    bool result = comparer.Equals(a, b);
+    return result;
+}
+
+bool r = EqualsCheck(new { Age = 5 }, new { Age = 5 });
+Console.WriteLine(r ? "YES" : "NO");
